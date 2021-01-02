b7619b4bcfdd78e4849bcc07a121ff4b3d463ef1
Jérémie Roquet <jroquet@arkanosis.net>
2021-01-02 19:46:32 +0100

    Parse pages' text and redirects

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,15 @@ use quick_xml::events::Event;
 
 enum Tag {
     Title,
+    Text,
     UserName,
     Other,
+}
+
+struct Page {
+    title: String,
+    text: Option<String>,
+    target: Option<String>,
 }
 
 pub fn version() -> &'static str {
@@ -16,6 +23,9 @@ pub fn run() {
     let mut xml_reader = quick_xml::Reader::from_reader(xml_stream);
     let mut buffer = Vec::new();
     let mut current_tag = Tag::Other;
+    let mut current_title: Option<String> = None;
+    let mut current_text: Option<String> = None;
+    let mut current_target: Option<String> = None;
     loop {
         match xml_reader.read_event(&mut buffer) {
             Ok(Event::Empty(ref event)) => {
@@ -25,8 +35,7 @@ pub fn run() {
                             match attribute {
                                 Ok(attribute) => {
                                     if attribute.key == b"title" {
-                                        println!("  => {}", std::str::from_utf8(&attribute.unescaped_value().unwrap()).unwrap());
-                                        // TODO FIXME
+                                        current_target = Some(std::str::from_utf8(&attribute.unescaped_value().unwrap()).unwrap().to_string());
                                     }
                                 }
                                 Err(_) => (), // ignore bad attribute in the dump
@@ -40,19 +49,51 @@ pub fn run() {
             Ok(Event::Start(ref event)) => {
                 match event.name() {
                     b"title" => current_tag = Tag::Title,
+                    b"text" => current_tag = Tag::Text,
                     b"ip" => current_tag = Tag::UserName,
                     b"username" => current_tag = Tag::UserName,
                     _ => current_tag = Tag::Other,
                 }
             },
-            Ok(Event::End(_)) => current_tag = Tag::Other,
+            Ok(Event::End(ref event)) => {
+                match event.name() {
+                    b"title" => current_tag = Tag::Other,
+                    b"text" => current_tag = Tag::Other,
+                    b"ip" => current_tag = Tag::Other,
+                    b"username" => current_tag = Tag::Other,
+                    b"page" => {
+                        let page = Page {
+                            title: current_title.unwrap(),
+                            text: current_text,
+                            target: current_target,
+                        };
+                        current_title = None;
+                        current_text = None;
+                        current_target = None;
+                        if page.target.is_some() {
+                            println!("Page: '{}', redirect to: {}", page.title, page.target.unwrap());
+                        } else {
+                            println!("Page: '{}', length: {}", page.title, page.text.unwrap().len());
+                        }
+                        current_tag = Tag::Other;
+                    },
+                    _ => current_tag = Tag::Other,
+                }
+            }
             Ok(Event::Text(ref event)) => {
                 match current_tag {
                     Tag::Title => {
                         match event.unescaped() {
                             Ok(ref buffer) => {
-                                println!("{}", std::str::from_utf8(buffer).unwrap());
-                                // TODO FIXME
+                                current_title = Some(std::str::from_utf8(buffer).unwrap().to_string());
+                            }
+                            Err(_) => (), // ignore encoding error in the dump
+                        }
+                    },
+                    Tag::Text => {
+                        match event.unescaped() {
+                            Ok(ref buffer) => {
+                                current_text = Some(std::str::from_utf8(buffer).unwrap().to_string());
                             }
                             Err(_) => (), // ignore encoding error in the dump
                         }
