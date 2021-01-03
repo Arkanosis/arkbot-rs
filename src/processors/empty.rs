9b8fb1faecde6636285f8d57101fc17f4e811094
Jérémie Roquet <jroquet@arkanosis.net>
2021-01-03 03:06:58 +0100

    Implement processor to detect empty pages

--- src/processors/empty.rs
+++ src/processors/empty.rs
@@ -1,23 +1,62 @@
 use crate::processors;
 use crate::wiki;
 
+use regex::Regex;
+
+use std::{
+    collections::HashSet,
+    fs::File,
+    io::{
+        BufWriter,
+        Write,
+    },
+};
+
 pub struct Empty {
-    // TODO
+    titles: Vec<String>,
+    empty: Regex,
+    ignore: HashSet<String>,
 }
 
 impl Empty {
     pub fn new() -> Self {
-        Empty {
-            // TODO
-        }
+        let mut result = Empty {
+            titles: Vec::new(),
+            empty: Regex::new(r"^[[:space:]]*$").unwrap(),
+            ignore: HashSet::new(),
+        };
+        result.ignore.insert("MediaWiki:Anonnotice".to_string());
+        result.ignore.insert("MediaWiki:Excontentauthor".to_string());
+        result.ignore.insert("MediaWiki:Reader-demographics-1-description".to_string());
+        result.ignore.insert("MediaWiki:Sitenotice".to_string());
+        result
     }
 }
 
 impl processors::Process for Empty {
     fn process(&mut self, page: &wiki::Page) {
-        // TODO
+        match page.target {
+            None => {
+                if let Some(text) = &page.text {
+                    if self.empty.is_match(&text) && !self.ignore.contains(&page.title) {
+                        self.titles.push(page.title.to_string());
+                    }
+                }
+            },
+            _ => (),
+        }
     }
     fn write_to_file(&mut self) {
-        // TODO
+        self.titles.sort();
+        const output_file: &str = "data/frwiki-empty_pages-latest.txt";
+        if let Ok(file) = File::create(output_file) {
+            let mut writer = BufWriter::new(file);
+            for title in self.titles.iter() {
+                writer.write(title.as_bytes());
+                writer.write(b"\n");
+            }
+        } else {
+            eprintln!("arkbot: unable to create file: '{}'", output_file);
+        }
     }
 }
